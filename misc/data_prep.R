063986178b4b63d5e04b07a4b6b322390f5b9efb
Cas Blaauw <38132585+CasBlaauw@users.noreply.github.com>
2021-09-15 23:05:06 +0200

    Update data_prep with positive/negative support

--- misc/data_prep.R
+++ misc/data_prep.R
@@ -1,13 +1,23 @@
 library(tidyverse)
 
-sites_path = 'XXX'
-sites <- read_tsv(sites_path)
+pos_path <- 'XXX'
+neg_path <- 'XXX'
+sites_pos <- read_tsv(pos_path)
+sites_neg <- read_tsv(neg_path)
 
-sites_unique <- sites %>% 
+sites_pos_unique <- sites_pos %>% 
   group_by(gene, sequence) %>% 
-  summarize(sites = paste(site, collapse = ' '))
+  summarize(sites = paste(site, collapse = ' ')) %>% 
+  mutate(gene = str_c(gene, '_pos'))
+
+sites_neg_unique <- sites_neg %>% 
+  group_by(gene, sequence) %>% 
+  summarize(sites = paste(site, collapse = ' ')) %>% 
+  mutate(gene = str_c(gene, '_neg'))
+
+sites_unique <- bind_rows(sites_pos_unique, sites_neg_unique)
 
 sites_unique_filtered <- sites_unique %>% 
   filter(nchar(sequence) < 10000)
 
-write_tsv(sites_unique_filtered, 'glycosites_unique_filtered.tsv')
+write_tsv(sites_unique_filtered, 'glycosites_combined_unique_filtered.tsv')
